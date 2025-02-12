0337abd3ad9a27fbf9f526b4005ca5a654b624b0
faima-ahmed <faimaahmeddiya@gmail.com>
2025-02-12 16:23:08 +0600

    style-code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./component/Navbar";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}>
         
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,20 +1,37 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav className="fixed m-4  top-0 right-0 left-0 shadow-lg z-50">
-            <div className="container px-4 flex justify-between items-center h-16">
-                <h3>Travel</h3>
-            </div>
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/gallery">Gallery</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/about">About</Link>
-            </div>
-
-        </nav>
-    );
+  return (
+    <nav className="fixed m-4  top-0 right-0 left-0 shadow-lg z-50 bg-white">
+      <div className="container px-4 flex justify-between items-center h-16">
+        <h3 className="text-2xl font-bold ">Travel</h3>
+        <div className="flex space-x-4 text-sm font-bold items-center">
+          <Link to="/" className="px-6 py-2 hover:bg-gray-600 hover:text-white">
+            Home
+          </Link>
+          <Link
+            to="/gallery"
+            className="px-6 py-2 hover:bg-gray-600 hover:text-white"
+          >
+            Gallery
+          </Link>
+          <Link
+            to="/contact"
+            className="px-6 py-2 hover:bg-gray-600 hover:text-white"
+          >
+            Contact
+          </Link>
+          <Link
+            to="/about"
+            className="px-6 py-2 hover:bg-gray-600 hover:text-white"
+          >
+            About
+          </Link>
+          <button className="py-2 px-6 border bg-gray-300">Login</button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
